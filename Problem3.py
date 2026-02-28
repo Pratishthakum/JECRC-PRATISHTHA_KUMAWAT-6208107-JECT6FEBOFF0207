6ba2b1fbf9c6082e4800cb2c5194c0af9a16a9ed
Pratishtha Kumawat <162327627+Pratishthakum@users.noreply.github.com>
2026-02-28 16:56:08 +0530

    Add files via upload

---
+++ Problem3.py
@@ -0,0 +1,19 @@
+#
+
+
+
+class Solution:
+
+    def department_patient_count(self, visits):
+        dept_count = {}
+        
+        for i in visits:
+            department = i["department"]
+            if department in dept_count:
+                dept_count[department] += 1
+            else:
+                dept_count[department] = 1
+
+        max_department = max(dept_count, key=dept_count.get)
+
+        return dept_count, max_department
