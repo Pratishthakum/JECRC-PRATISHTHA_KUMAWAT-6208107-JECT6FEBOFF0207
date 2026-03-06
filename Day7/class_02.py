29a6d360e8deacf7f09729b67e55668e7e36a6d1
Pratishtha Kumawat <162327627+Pratishthakum@users.noreply.github.com>
2026-03-06 23:03:00 +0530

    class_02.py

--- Day7/class_02.py
+++ Day7/class_02.py
@@ -1,78 +1,79 @@
 class Car:
-
-    wheelers=4
-    engine="Petrol"
-    base_speed="40Km/hr"
-    max_speed="120km/hr"
-    gears=4
-
-TATA=Car()
-TATA.engine=["Petrol","Diesel","EV"]
-TATA.airbags=True
-TATA.no_of_airbags=4
-TATA.base_budget='2L'
-TATA.no_of_varient=12
-
-
-def __init__(self, air_bags,security,base_budget,varient,total_sale):
-    self.air_bags=air_bags
-    self.security=security
-    self.base_budget=base_budget
-    self.varient=varient
-    self.total_sale=total_sale
-
-
+    ## Below variables are known as "Properties/States/Members".
+    wheelers = 4
+    engine = 'Petrol'
+    base_speed = '40kmph'
+    max_speed = '120kmph'
+    gears = 4
+    
+    def __init__(self, air_bags, security, base_budget, varient, total_sale):
+        self.air_bags = air_bags
+        self.security = security
+        self.base_budget = base_budget
+        self.varient = varient
+        self.total_sale = total_sale
+    
     @classmethod
-    def update_gears(cls,new_gears):
-        cls.gears=new_gears
-        print(f"no of gears: {cls.gears}")
-
-def display_properties(self):
-    print(
-        {
+    def update_gears(cls, new_gears):
+        cls.gears = new_gears
+        print(f'No of Gears: {cls.gears}')
+    
+    ## Functionality
+    def display_properties(self):
+        print({
+            'wheelers': self.wheelers,
+            'engine': self.engine,
+            'base_speed': self.base_speed,
+            'max_speed': self.max_speed,
+            'gears': self.gears,
             'air_bags': self.air_bags,
-            'security':self.security,
-            'base_budget':self.base_budget,
-            'varient':self.varient,
-            'total_sale':self.total_sale
-        }
-    )
+            'security': self.security,
+            'base_budget': self.base_budget,
+            'varient': self.varient,
+            'total_sale': self.total_sale
+        })
+    
+    def update_base_speed(self, new_speed):
+        self.base_speed = new_speed
+        print(f'New Base Speed: {self.base_speed}')
+        
+    def update_max_speed(self, new_speed):
+        self.max_speed = new_speed
+        print(f'New Max Speed: {self.max_speed}')
 
-def  update_base_speed(self, new_base_speed):
-    self.base_speed=new_base_speed
-    print(f"New base speed : {self.base_speed}")
 
-def update_max_speed(self,new_speed):
-    self.max_speed=new_speed
-    print(f"New max speed:{self.max_speed} ")
-
-TATA=Car(True,'level 5','2l',12,10000000)
-
+TATA = Car(True, 'Level 5', '2L', 12, 100000)
+print('Properties before updation:')
 TATA.display_properties()
-Mahindra=Car(True,'level 4','4l', 20, 2500000)
-Mahindra.display_properties()
 print()
 
-
-print("properties after updation")
-TATA.update_base_speed("6okm/hr")
-TATA.update_max_speed("130km/hr")
+print('Properties after updation:')
+TATA.update_base_speed('60kmph')
+TATA.update_max_speed('160kmph')
 TATA.update_gears(5)
 TATA.display_properties()
 
 
 
+mahindra = Car(True, 'Level 4', '4L', 22, 250000)
+print()
+print('Mahindra: ')
+mahindra.display_properties()
 
 
 
-#constructor---magic method (__init__)
-#used to initialize a state of object
+# self.prop_name
 
-# class ClassName:
-#     properties
+## constructor (__init__)
 
-#     def __init__(self,arg1,arg2,arg3,.......,argn)
-#         self.arg1=arg1
-#         self.arg2=arg2
-#         self.argn=argn
-
+'''
+class ClassName:
+    properties
+    
+    def __init__(self, arg1, arg2, arg3, ..., argn):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+        ...
+        self.argn = argn
+'''
